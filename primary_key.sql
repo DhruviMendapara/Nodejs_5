67d40180bafab0b18805b664bcc8bfa0a909debb
DhruviMendapara <106226235+DhruviMendapara@users.noreply.github.com>
2022-10-12 18:23:00 +0530

    12-10-2022

---
+++ primary_key.sql
@@ -0,0 +1,20 @@
+-- create table categories(
+--     Id Int Primary key,
+--     Name varchar(50)
+-- );
+
+-- Insert into categories values(101,"Mobile");
+-- Insert into categories values(102,"Tv");
+
+-- create table mybag(
+--     color varchar(30),
+--     size int,
+--     constraint pk_mybag Primary key(color,size)
+-- );
+
+Insert into mybag values("red",30);
+Insert into mybag values("g",30);
+Insert into mybag values("b",30);
+Insert into mybag values("red",25);
+
+
