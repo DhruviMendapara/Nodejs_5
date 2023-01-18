b10dd94ecba6571699843430253a33427c7471ce
DhruviMendapara <106226235+DhruviMendapara@users.noreply.github.com>
2023-01-18 18:58:13 +0530

    eshop

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
-// const express = require("express");
-// const app = express();
-// const PORT = 3003;
+const express=require("express");
+const port=3300;
 
-// app.get("/", (req, res) => {
-//   res.json({ msg: "Hello" });
-//   console.log("Connected");
-// });
+const cors=require("cors");
+const mongoose=require("mongoose");
 
-// app.get("/login", (req, res) => {
-//   res.json({ msg: "Login" });
-// });
+const Categorydata=require("./data/category");
+const Category=require("./models/CategoryModel");
 
-// app.post("/login", (req, res) => {
-//   res.json({ msg: "Login Post" });
-// });
-// app.listen(PORT, () => {
-//   console.log(`server listening port ${PORT}`);
-// });
+mongoose.set("strictQuery",true);
 
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+
+
+mongoose
+.connect("mongodb+srv://admin:admin@crud.zxmps4i.mongodb.net/eshop?retryWrites=true&w=majority",{
+// .connect("mongodb://localhost:27017/eshop",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+.then(()=>console.log("connected"))
+.catch((err)=>{
+    console.log(err);
+})
+
+app.post("/insert_records",(req,res)=>{
+    console.log(Categorydata.Category);
+    const result=Category.insertMany(Categorydata.Category);
+    res.send(result)
+})
+
+// app.get("/",(req,res)=>{
+//     res.json({"msg":"connected"});
+// })
+
+
+app.listen(port,()=>{
+    console.log(`sever connected :${port}`);
+})
